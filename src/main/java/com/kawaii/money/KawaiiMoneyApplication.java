89d3c6349b1962a82405bba5cd85afb7c32810c2
Emanuel Sotomayor <emanuel.sotomayor.sec@gmail.com>
2024-06-20 20:09:21 -0300

    Initial project

---
+++ src/main/java/com/kawaii/money/KawaiiMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.kawaii.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KawaiiMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KawaiiMoneyApplication.class, args);
+	}
+
+}
